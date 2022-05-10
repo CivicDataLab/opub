2deb840b9a0d84236ff0da9d6030655329c87c03
bhavabhuthi <bhavabhuthi@civicdatalab.in>
2022-05-10 11:14:33 +0530

    Add reference to new components

--- components/pages/home/index.ts
+++ components/pages/home/index.ts
@@ -3,3 +3,6 @@ export { default as HomeAbout } from './HomeAbout';
 export { default as HomeHighlight } from './HomeHighlight';
 export { default as HomeStates } from './HomeStates';
 export { default as HomeQuiz } from './HomeQuiz';
+export { default as HomeExplore } from './HomeExplore';
+export { default as HomeBanner } from './HomeBanner';
+export { default as HomeDataCarousal } from './HomeDataCarousal';
