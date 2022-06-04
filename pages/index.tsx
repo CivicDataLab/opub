15003aa457bfdec257f0e64dfc20e3d55fa06632
pixeledcode <ashoaib540918@gmail.com>
2022-06-04 12:31:16 +0530

    carousel added

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import {
   HomeStates,
 } from 'components/pages/home';
 import { Seo } from 'components/common';
+import HomeCarousel from 'components/pages/home/HomeCarousel';
 
 export default function Home() {
   return (
@@ -23,6 +24,7 @@ export default function Home() {
         <HomeAbout />
         <HomeStates />
         <HomeQuiz />
+        <HomeCarousel />
       </HomePage>
     </>
   );
