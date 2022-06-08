28bcd1f28241d494e5cce51cf6d3d41862d45c48
bhavabhuthi <bhavabhuthi@civicdatalab.in>
2022-06-08 19:07:18 +0530

    Delete unused variable in Home Partners component

--- components/pages/home/HomePartners/HomePartners.tsx
+++ components/pages/home/HomePartners/HomePartners.tsx
@@ -28,16 +28,6 @@ const HomePartners = (partnersData) => {
     </svg>
   );
 
-  const partner = {
-    name: 'Google',
-    datasetsNumber: '199,812,341',
-    avgRating: '4.5',
-    downloads: '458762',
-    content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
-    link: '',
-  };
-
   return (
     <About>
       <div className="container">
